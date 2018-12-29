82d6fce4af474b10dc4cc126f0e9a28c82cf5871
“Waixi00008” <xiaobing00008@gmail.com>
2018-12-29 08:02:33 +0800

    feat: 静态html页面

--- server/index.js
+++ server/index.js
@@ -1,6 +1,10 @@
 const Koa = require('koa')
 const app = new Koa()
+const {
+  normal
+} = require('./tpl/index.js')
 app.use(async (ctx, next) => {
-  ctx.body = '电影预告'
+  ctx.type = 'text/html; charset=utf-8'
+  ctx.body = normal
 })
 app.listen(3000)
