ef331c319b763145db16b1cff9376431a80e7a6f
“Waixi00008” <xiaobing00008@gmail.com>
2018-12-29 13:13:45 +0800

    feat：首页

--- server/index.js
+++ server/index.js
@@ -1,7 +1,10 @@
 const Koa = require('koa')
 const views = require('koa-views')
+const cors = require('koa2-cors');
 const { resolve } = require('path')
 const app = new Koa()
+
+app.use(cors())
 
 app.use(views(resolve(__dirname,'./views'), {
   extension: 'pug'
